4e7a9940be4fd51c43336208b30679f0eccf964c
ShoorEkko <131442329+ShoorEkko@users.noreply.github.com>
2025-06-26 19:20:46 +0800

    Update

---
+++ api/speak.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+    if (req.method !== "POST") {
+      return res.status(405).json({ error: "Only POST allowed" });
+    }
+  
+    const { agentId, inputText } = req.body;
+  
+    const response = await fetch(`https://api.d-id.com/agents/${agentId}/chat`, {
+      method: "POST",
+      headers: {
+        "Authorization": "Basic ZXZhbmRlci5mb3h4QGRzaXRpcC5jb20:3vPGhYgaZ6NDukM8jUTKp", // OR Bearer API_KEY
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        type: "text",
+        input: inputText
+      })
+    });
+  
+    const data = await response.json();
+    res.status(response.status).json(data);
+  }
+  
